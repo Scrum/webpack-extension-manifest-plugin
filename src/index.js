15c72ea63a7378d2822b2228cfb048be2366ae17
Ivan Demidov <scrum@list.ru>
2017-12-06 10:30:05 +0300

    style: change tab to space

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import path from 'path';
 import merge from 'deepmerge';
 
 export default class WebpackExtensionManifestPlugin {
-	constructor(baseConfig, extendConfig) {
-		this.baseConfig = baseConfig;
-		this.extendConfig = extendConfig;
-	}
+    constructor(baseConfig, extendConfig) {
+        this.baseConfig = baseConfig;
+        this.extendConfig = extendConfig;
+    }
 
-	apply(compiler) {
-		compiler.plugin('emit', (compilation, callback) => {
-			const json = JSON.stringify(merge(this.baseConfig, this.extendConfig));
-			const file = path.join(compilation.outputOptions.path, 'manifest.json');
-			fs.writeFile(file, json, callback);
-		});
-	}
+    apply(compiler) {
+        compiler.plugin('emit', (compilation, callback) => {
+            const json = JSON.stringify(merge(this.baseConfig, this.extendConfig));
+            const file = path.join(compilation.outputOptions.path, 'manifest.json');
+            fs.writeFile(file, json, callback);
+        });
+    }
 }
